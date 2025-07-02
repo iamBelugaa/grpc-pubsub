639e72ac1e6ce35699640edfddf58f24fdc2762c
iamBelugaa <iamnilotpaldeka@gmail.com>
2025-07-02 14:05:39 +0530

    - implemented Subscribe to allow consumers to listen to topics via gRPC stream.
    - added readFromStream to handle continuous message reception in a separate goroutine.

--- internal/consumer/consumer.go
+++ internal/consumer/consumer.go
@@ -3,6 +3,7 @@ package consumer
 import (
 	"context"
 	"fmt"
+	"io"
 
 	"github.com/google/uuid"
 	"go.uber.org/zap"
@@ -37,6 +38,34 @@ func New(logger *zap.SugaredLogger, brokerAddr string) (*consumer, error) {
 	// Create a new PubSub client using the connection.
 	client := pubsubpb.NewPubSubServiceClient(conn)
 	return &consumer{id: uuid.New().String(), log: logger, conn: conn, client: client}, nil
+}
+
+// Subscribe connects to a topic and starts reading messages from the stream.
+func (s *consumer) Subscribe(context context.Context, topic string) {
+	s.log.Infow("subscribe request initiated", "topic", topic, "subscriberId", s.id)
+
+	stream, err := s.client.Subscribe(context, &pubsubpb.SubscribeRequest{
+		SubscriberId: s.id,
+		Topic:        topic,
+	})
+
+	// Handle subscription errors.
+	if err != nil {
+		if status, ok := status.FromError(err); ok {
+			s.log.Infow(
+				"failed to subscribe",
+				"statusCode", status.Code(),
+				"message", status.Message(),
+				"topic", topic, "subscriberId", s.id,
+			)
+		} else {
+			s.log.Infow("failed to subscribe", "error", err, "topic", topic, "subscriberId", s.id)
+		}
+		return
+	}
+
+	// Start reading from the stream.
+	go s.readFromStream(context, stream)
 }
 
 // Unsubscribe disconnects from a topic.
@@ -80,4 +109,31 @@ func (s *consumer) Close() error {
 
 	s.log.Infow("closed consumer service successfully", "subscriberId", s.id)
 	return nil
+}
+
+// readFromStream continuously reads messages from the gRPC stream.
+func (s *consumer) readFromStream(context context.Context, stream grpc.ServerStreamingClient[pubsubpb.PayloadStream]) {
+	for {
+		select {
+		case <-context.Done():
+			s.log.Infow("context cancelled - stopping stream read", "subscriberId", s.id)
+			stream.CloseSend()
+			s.log.Infow("stream closed after context cancellation", "subscriberId", s.id)
+
+		default:
+			// Receive the next message from the stream.
+			msg, err := stream.Recv()
+			if err != nil {
+				if err == io.EOF {
+					s.log.Infow("stream closed by server", "subscriberId", s.id)
+					return
+				} else {
+					s.log.Infow("error receiving message from stream", "error", err, "subscriberId", s.id)
+					continue
+				}
+			}
+
+			s.log.Infow("message received", "topic", msg.Topic, "payload", string(msg.Payload), "subscriberId", s.id)
+		}
+	}
 }
